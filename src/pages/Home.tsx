378163cf0661458d6aa080408c69af607b81fafd
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-08-10 11:04:45 +0100

    style: init migration from tailwindCSS to Chakra UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,38 @@
+import { Box } from "@chakra-ui/react";
+
 const Home = () => {
   return (
     <>
-      <div>
-        <div className="max-w-full min-h-screen mx-auto flex justify-center items-center">
-          <div className="p-4 lg:p-10">
-            <div className="text-center px-4 lg:px-40 py-20">
+      <Box>
+        <Box maxW="100vw" minH="100vh" mx="auto" bg="gray.50">
+          <Box p={{ base: "1rem", lg: "2.5rem" }}>
+            <Box
+              py={"5rem"}
+              px={{ base: "1rem", lg: "10rem" }}
+              textAlign={"center"}
+            >
               <img
                 className="object-contain h-28 mx-auto rounded-full"
                 src="src/assets/img/nigeria_flag.jpg"
               />
-              <p className="text-6xl font-archivoBlack leading-snug text-ngDarkblue font-black pt-10">
-                <span className="text-gray-600">#1</span> The
+              <p className="text-6xl font-archivoBlack leading-snug text-ngDarkblue font-black pt-10 lg:px-4">
+                <span className="text-gray-600 text-4xl">#1</span> The
                 <span className="text-green-800"> Nigeria </span> Food Database
                 API
               </p>
-              <p className="text-xl font-albertSans leading-relaxed text-gray-600 pt-10 lg:px-24">
+              <p className="text-xl font-albertSans leading-relaxed text-gray-700 pt-10 lg:px-24 ">
                 Explore a rich collection of Nigerian recipes, ingredients,
                 <br />
                 and culinary traditions with our comprehensive <br />
                 Nigeria Food API database.
               </p>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-full min-h-screen bg-gradient-to-r from-green-900 via-green-800 to-green-900">
-          <div className="p-4 lg:p-10"></div>
-        </div>
-      </div>
+            </Box>
+          </Box>
+        </Box>
+        <Box className="max-w-full min-h-screen bg-gradient-to-r from-green-900 via-green-800 to-green-900">
+          <Box className="p-4 lg:p-10"></Box>
+        </Box>
+      </Box>
     </>
   );
 };

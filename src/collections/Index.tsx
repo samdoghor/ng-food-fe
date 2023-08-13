e1de72b75eb148df28fee6d30db7b31a256106a6
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-08-13 07:10:34 +0100

    feat: definition of basic routes

--- src/collections/Index.tsx
+++ src/collections/Index.tsx
@@ -1,3 +1,7 @@
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Privacy from "../pages/Privacy";
+import Terms from "../pages/Terms";
+import NotFound from "../pages/NotFound";
 
-export { Home };
+export { Home, About, Privacy, Terms, NotFound };
---
+++ src/pages/Terms.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Terms = () => {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+};
+
+export default Terms;
